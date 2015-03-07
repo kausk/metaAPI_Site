a8c42620a31df6de00aa4a0171828785dedbed36
Kaushik Kasi <kaushik.kasi@gmail.com>
2015-03-07 08:08:35 -0800

    login page

--- flask-boot/app.py
+++ flask-boot/app.py
@@ -1,8 +1,12 @@
 # import the Flask class from the flask module
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect, url_for
 import requests
 import simplejson as json
 from urllib2 import Request, urlopen
+from pymongolab import MongoClient
+
+##initializing the mongoclient
+MongoClient('K4o23oEbq20nb1tS-1kmkAqGuTmE8aed')
 
 # create the application object
 app = Flask(__name__)
@@ -17,12 +21,45 @@ def home():
 def welcome():
     return render_template('welcome.html')  # render a template
 
+@app.route('/testdb')
+
 @app.route('/apiget')
 def get_data():
+
 	request = Request('http://private-59c72-metapi.apiary-mock.com/notes') ##get content from apiary
-	response_body = urlopen(request).read()
+	response_body = urlopen(request).read().info()
 	return response_body
 
+# route for handling the login page logic
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    error = None
+    if request.method == 'POST':
+        if request.form['username'] != 'admin' or request.form['password'] != 'admin':
+            error = 'Invalid Credentials. Please try again.'
+        else:
+            return redirect(url_for('home'))
+    return render_template('login.html', error=error)
+
+
+    'https://api.mongolab.com/api/1/databases?apiKey=K4o23oEbq20nb1tS-1kmkAqGuTmE8aed'
+    ##mongolab url
+@app.route('/apipost')
+def post_data():
+
+	values = """
+	  {
+	    "title": "Buy cereal and bread for lunch."
+	  }
+	"""
+
+	headers = {
+	  'Content-Type': 'application/json'
+	}
+	request = Request('http://private-59c72-metapi.apiary-mock.com/notes', data=values, headers=headers)
+
+	response_body = urlopen(request).read()
+	return response_body
 
 
 # start the server with the 'run()' method
